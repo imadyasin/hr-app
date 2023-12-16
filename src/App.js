3200375a484bb7147b1f8e8d526dd74fbf3d69b0
Imad <imadyasin07@gmail.com>
2023-12-16 19:20:03 +0530

    change in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Outlet} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Contact from './components/Contact';
 import Mainpage from './components/Mainpage';
 import Navbar from './components/Navbar';
